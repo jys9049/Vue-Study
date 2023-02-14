ae3221c3e171dc213967c943a0ce39e7a0369c49
jys9049 <github.com.com/jys9049>
2023-02-14 18:44:20 +0900

    1차 리펙토링

---
+++ vue-todo-test/src/components/TodoInput.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="inputContainer">
+        <input id="text" type="text" v-model="todoText" v-on:keypress.enter="sendEvent">
+        <button v-on:click="sendEvent">add</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todoText: ''
+        }
+    },
+    methods: {
+        sendEvent() {
+            if (this.todoText.match(/\s/g) || this.todoText === '') {
+                return this.todoText = ''
+            }
+            this.$emit('addBtnClick', this.todoText)
+            this.todoText = ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+input {
+    width: 100%;
+    height: 40px;
+    border: 1px solid #c9c9c9;
+    padding-left: 10px;
+}
+
+button {
+    position: absolute;
+    right: 15px;
+    width: 30px;
+    height: 30px;
+    background: white;
+    border: none;
+    cursor: pointer;
+}
+</style>
---
+++ vue-todo-test/src/components/TodoList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="todoItemContainer">
+        <li class="todoItem" v-for="item in todo" :key="item.id">
+            <input type="checkbox" name="check" id="check" v-on:click="checkedClick(item.id)"
+                v-bind:checked="item.checked" />
+            <h3 v-bind:class="{ 'finishText': item.checked }">{{ item.text }}</h3>
+            <button v-on:click="deleteClick(item.id)"> X </button>
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['todo'],
+    methods: {
+        checkedClick(id) {
+            this.$emit('checked', id)
+        },
+        deleteClick(id) {
+            this.$emit('delete', id)
+        }
+    }
+}
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  list-style: none;
+}
+
+li>input[type="checkbox"] {
+  width: 25px;
+  height: 25px;
+  border: 1px solid #c9c9c9;
+  margin: 0;
+}
+
+li>button {
+  width: 25px;
+  height: 25px;
+  background: white;
+  border: 1px solid #c9c9c9;
+  border-radius: 4px;
+}
+
+.finishText {
+  color: red;
+}
+</style>
